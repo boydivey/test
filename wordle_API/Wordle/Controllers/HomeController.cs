06c101cc2f2dd36fcb4ca8cdc57baed081a63e82
Ivey, Boyd <Boyd.Ivey@henryschein.com>
2022-05-25 17:42:43 -0600

    Test

---
+++ wordle_API/Wordle/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestCoreAPI.Controllers
+
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WordleController : ControllerBase
+    {
+        [HttpGet(WordleController = "Wordle")]
+        //public String Wordle()
+        
+            // String result = "This is taking longer than expected. :( -- Sadness...";
+
+
+            Random random = new Random();
+
+            public WordleController Method()
+            {
+                string[] myString = File.ReadAllLines("C:\\Users\\bivey\\source\\repos\\wordle-react\\wordle_API\\Controllers\\Words.txt");
+                return myString[random.Next(myString.Length)];
+
+             }
+
+        }
+}
+
---
+++ wordle_API/Wordle/Service/CodeFile.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+//using TestCoreAPI.DTO;
+
+namespace TestCoreAPI.Service
+{
+    public class WordleService
+    {
+
+
+
+    }
+}
+
+
+
